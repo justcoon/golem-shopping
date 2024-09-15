6519ac149f191541ac14740d8eea531648a55263
Peter Kotula <peto.kotula@yahoo.com>
2024-09-15 17:55:13 +0200

    order, product, pricing

---
+++ pricing/src/lib.rs
@@ -0,0 +1,48 @@
+mod bindings;
+
+use crate::bindings::exports::golem::it::api::*;
+use crate::bindings::golem::api::host::*;
+use std::cell::RefCell;
+use std::env;
+
+use rand::prelude::*;
+
+struct Component;
+
+thread_local! {
+    static STATE: RefCell<Option<Pricing>> = const { RefCell::new(None) };
+}
+
+fn with_state<T>(f: impl FnOnce(&mut Pricing) -> T) -> T {
+    STATE.with_borrow_mut(|state| {
+        if state.is_none() {
+            let worker_name = env::var("WORKER_NAME").expect("WORKER_NAME must be set");
+            let product =
+                Pricing { asset_id: worker_name, msrp_prices: vec![], list_prices: vec![] };
+            *state = Some(product);
+        }
+
+        f(state.as_mut().unwrap())
+    })
+}
+
+// Here, we declare a Rust implementation of the `ShoppingCart` trait.
+impl Guest for Component {
+    fn initialize_pricing(msrp_prices: Vec<PricingItem>, list_prices: Vec<PricingItem>) -> () {
+        with_state(|state| {
+            println!("Initializing pricing {}", state.asset_id);
+            state.msrp_prices = msrp_prices;
+            state.list_prices = list_prices;
+        });
+    }
+
+    fn get() -> Option<Pricing> {
+        STATE.with_borrow(|state| {
+            println!("Getting pricing");
+
+            state.clone()
+        })
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
---
+++ product/src/lib.rs
@@ -0,0 +1,52 @@
+mod bindings;
+
+use crate::bindings::exports::golem::it::api::*;
+use crate::bindings::golem::api::host::*;
+use std::cell::RefCell;
+use std::env;
+
+use rand::prelude::*;
+
+struct Component;
+
+thread_local! {
+    static STATE: RefCell<Option<Product>> = const { RefCell::new(None) };
+}
+
+fn with_state<T>(f: impl FnOnce(&mut Product) -> T) -> T {
+    STATE.with_borrow_mut(|state| {
+        if state.is_none() {
+            let worker_name = env::var("WORKER_NAME").expect("WORKER_NAME must be set");
+            let product = Product {
+                product_id: worker_name,
+                name: "undefined".to_string(),
+                description: "undefined".to_string(),
+            };
+            *state = Some(product);
+        }
+
+        f(state.as_mut().unwrap())
+    })
+}
+
+// Here, we declare a Rust implementation of the `ShoppingCart` trait.
+impl Guest for Component {
+    fn initialize_product(name: String, description: String) -> () {
+        with_state(|state| {
+            println!("Initializing product {}", state.product_id);
+
+            state.name = name;
+            state.description = description;
+        });
+    }
+
+    fn get() -> Option<Product> {
+        STATE.with_borrow(|state| {
+            println!("Getting product");
+
+            state.clone()
+        })
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
